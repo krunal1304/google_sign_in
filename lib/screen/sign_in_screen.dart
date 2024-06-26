adff9f12da8e2a6c97367eade6969202b7af01e0
krunal1304 <koratkrunal1997@gmail.com>
2024-06-26 15:31:15 +0530

    createproject

---
+++ lib/screen/sign_in_screen.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatefulWidget {
+
+  const SignInScreen({super.key});
+
+  @override
+  State<SignInScreen> createState() => SignInScreenDemo();
+
+}
+
+class SignInScreenDemo extends State<SignInScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+}
